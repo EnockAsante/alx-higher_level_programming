4cd6590468f6cae7239f8d681b01df625ff5e46f
Enock <asanteenoch10@gmail.com>
2024-02-08 00:33:06 +0100

    update

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,10 +6,10 @@ numb_dub = abs(number)
 if numb_dub > 0:
     rem = int(numb_dub % 10)
 
-print(f"last digit of {number} is {rem}", end="")
+print(f"last digit of {number} is {rem} ", end="")
 if rem > 5:
-    print(" and is greater than 5")
+    print("and is greater than 5")
 elif rem == 0:
-    print(" and is 0")
+    print("and is 0")
 elif rem < 6:
-    print(" and is less than 6 and not 0")
+    print("and is less than 6 and not 0")
