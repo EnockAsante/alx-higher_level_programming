25185b17a03b69977f32433f3cb431fd5e09dc60
Enock <asanteenoch10@gmail.com>
2024-02-08 00:21:08 +0100

    update

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+rem = 0
+numb_dub = abs(number)
+if numb_dub > 0:
+    rem = int(numb_dub % 10)
+
+print (f"last digit of {number} is {rem}")
+if rem > 5:
+    print("and is greater than 5")
+elif rem == 0:
+    print("and is 0")
+elif rem < 6:
+    print("and is less than 6 and not 0")
