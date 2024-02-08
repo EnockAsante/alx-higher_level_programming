19cfdc6c8ecf67a51f5218e1dba0431a9b6762cd
Enock <asanteenoch10@gmail.com>
2024-02-08 21:29:15 +0100

    update

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
-for i in range(10):
-    for j in range(i + 1, 10):
-        print("{}{}".format(i,j), end=", " if i < 8 else "\n")
+for i in range(100):
+    print("{:02}".format(i), end = ", " if i < 99 else "\n")
