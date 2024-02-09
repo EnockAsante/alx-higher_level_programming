b0f2a661597f660593d16f89d8268596e7ed671c
Enock <asanteenoch10@gmail.com>
2024-02-09 19:51:10 +0100

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 from sys import argv
+
 if __name__ == "__main__":
     counter = 0
-    print("{:d} arguments:".format(len(argv) - 1))
+    print("{:d} arguments".format(len(argv) - 1), end = ":\n" if len(argv) - 1 \
+                                                                 >= 1 else
+    ".\n")
     for i in argv:
         if counter > 0:
             print("{:d}: {}".format(counter, i))
