06f336e4ce40211a969cc61f59e99abfc4f0f496
Enock <asanteenoch10@gmail.com>
2024-02-09 21:06:28 +0100

    update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,11 @@
 import marshal
 
 if __name__ == "__main__":
-    with open("/home/gadget/Downloads/hidden_4.pyc", 'rb') as f:
+    with open("hidden_4.pyc", 'rb') as f:
         code_obj = marshal.load(f)
         print(code_obj)
+    names = [name for name in dir(code_obj) if not name.startswith('__')]
+    names.sort()
+
+    for name in names:
+        print(name)
