4f1b221a1bbfa8c20dbbac23b4aa8d80979a837b
Enock <asanteenoch10@gmail.com>
2024-02-09 19:41:22 +0100

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 from sys import argv
-
-counter = 0
-print("{:d} arguments :".format(len(argv) - 1))
-for i in argv:
-    if counter > 0:
-        print("{:d}: {}".format(counter, i))
-    counter += 1
+if __name__ == "__main__":
+    counter = 0
+    print("{:d} arguments :".format(len(argv) - 1))
+    for i in argv:
+        if counter > 0:
+            print("{:d}: {}".format(counter, i))
+        counter += 1
