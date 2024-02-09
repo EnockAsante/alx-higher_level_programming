0fd05c9e1b98d66738693fb1a49c47bed36312a4
Enock <asanteenoch10@gmail.com>
2024-02-09 16:45:57 +0100

    update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0
+import add_0 as add
 
 if __name__ == "= __main__":
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}\n".format(a, b, add_0.add(a, b)))
+    print("{:d} + {:d} = {:d}\n".format(a, b, add(a, b)))
