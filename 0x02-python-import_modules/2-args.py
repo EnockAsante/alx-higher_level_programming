91fccbd98a8f71fee1815e23e868fc0597e74c6e
Enock <asanteenoch10@gmail.com>
2024-02-09 20:14:26 +0100

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,10 @@ from sys import argv
 
 if __name__ == "__main__":
     counter = 0
-    print("{:d} arguments".format(len(argv) - 1), end = ":\n" if len(argv) - 1 \
-                                                                 >= 1 else
-    ".\n")
+    len_ = len(argv) - 1
+
+    print("{:d} argument".format(len_), end="s:\n" if len_ > 1 else "s:\n" if
+    len_ == 0 else ".\n")
     for i in argv:
         if counter > 0:
             print("{:d}: {}".format(counter, i))
