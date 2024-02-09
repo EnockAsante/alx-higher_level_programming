36ec36d0d5b4d931953019084a725eb9f680ba2a
Enock <asanteenoch10@gmail.com>
2024-02-09 21:22:23 +0100

    update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import marshal
+import hidden_4
+
+def discovr():
+    name = dir(hidden_4)
+    for i in name:
+        if i[:2] != '__':
+            print("{:s}".format(i))
+
 
 if __name__ == "__main__":
-    with open("hidden_4.pyc", 'rb') as f:
-        code_obj = marshal.load(f)
-        print(code_obj)
-    names = [name for name in dir(code_obj) if not name.startswith('__')]
-    names.sort()
-
-    for name in names:
-        print(name)
+    discovr()
