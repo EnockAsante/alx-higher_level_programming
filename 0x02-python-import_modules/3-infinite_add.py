2b7129d64a48ccd25da00dc6712866051f8f0e7c
Enock <asanteenoch10@gmail.com>
2024-02-09 20:30:26 +0100

    update

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    counter = 0
+    sum = 0
+    for i in argv:
+        if counter > 0:
+            sum += ord(i)
+        counter += 1
+    print("{:d}".format(sum))
