720196f570c0f697ae6fde92157b9ab4f7600f2e
Enock <asanteenoch10@gmail.com>
2024-02-09 19:40:03 +0100

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 from sys import argv
 
 counter = 0
-print("{:d} arguments:".format(len(argv) - 1))
+print("{:d} arguments :".format(len(argv) - 1))
 for i in argv:
     if counter > 0:
         print("{:d}: {}".format(counter, i))
