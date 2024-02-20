7a3b8110f2b0e3ea0c106e14a0edbe0794d60069
Enock <asanteenoch10@gmail.com>
2024-02-20 22:02:37 +0100

    update

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list = [], number = 0):
-    return {k: (v * number) if my_list else {} for k, v in my_list.items()}
+    return [map(lambda x: x * number, my_list) if my_list else []]
