59a5f1dfcf74cb1e391e93b005c3b83e37b3a675
Enock <asanteenoch10@gmail.com>
2024-02-20 16:27:48 +0100

    update

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,6 +4,6 @@ def update_dictionary(a_dictionary, key, value):
         if k == key:
             k: value
             return a_dictionary
-        else:
+        elif k != key and i == len(a_dictionary) - 1:
             a_dictionary[key] = value
     return a_dictionary
