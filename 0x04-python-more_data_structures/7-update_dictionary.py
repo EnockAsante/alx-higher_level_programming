3c72bb3d838269cde5025baf8b59f4d195f4d2d1
Enock <asanteenoch10@gmail.com>
2024-02-20 16:33:18 +0100

    update

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary = {k: value if k == key else v for k, v in a_dictionary.items()}
     for k, v in a_dictionary.items():
-        if k != key:
+        if k == key:
+            k: value
+        else:
             a_dictionary[key] = value
             return a_dictionary
     return a_dictionary
