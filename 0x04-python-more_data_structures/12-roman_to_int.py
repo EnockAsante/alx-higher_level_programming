2b79dfd4c1fcf0a6ced016b4f703f4c57d5e7077
Enock <asanteenoch10@gmail.com>
2024-02-20 22:52:21 +0100

    update

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_dict = {"M": 1000, "D": 500, "C": 100,  "L": 50, "X": 10, "V": 5,
+                  "I": 1}
+    n = []
+    s = 0
+    if roman_string:
+        for l in roman_string:
+            for k, v in roman_dict.items():
+                if l == k:
+                    n.append(v)
+        for i, num in enumerate(n):
+            if i > 0 and num > n[i - 1]:
+                s -= num
+            else:
+                s += num
+        return abs(s)
+    else:
+        return 0
