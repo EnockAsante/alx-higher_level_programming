bbfc7bad2204b60b8ca9a67e29fd4ba73d3b1f34
Enock <asanteenoch10@gmail.com>
2024-02-20 21:30:13 +0100

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,3 @@ def best_score(a_dictionary):
     n = "None" if not a_dictionary else sorted(a_dictionary.keys(),
                                                reverse=1)[0]
     return n
-
-a = {'a':700, 'c':"e", 'e':20}
-print(best_score(a))
-print(a)
