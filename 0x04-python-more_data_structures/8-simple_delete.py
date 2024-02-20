01ea1da4cf39d35459960c3a67417f6b02a452f9
Enock <asanteenoch10@gmail.com>
2024-02-20 19:31:21 +0100

    update

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key = ""):
+    oc_counts = 0
     for k, v in a_dictionary.items():
         if k == key:
-            del a_dictionary[key]
+            oc_counts += 1
+    while oc_counts > 0:
+        del a_dictionary[key]
+        oc_counts -= 1
     return a_dictionary
