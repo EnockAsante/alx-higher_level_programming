f649f227530552e53ccf513363c0fefdf415a41a
Enock <asanteenoch10@gmail.com>
2024-02-20 21:47:43 +0100

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     l_i = len(a_dictionary) - 1
-    n = "None" if not a_dictionary else sorted(a_dictionary, reverse=True)[l_i]
+    n = "None" if not a_dictionary else sorted(a_dictionary)[l_i]
     return n
