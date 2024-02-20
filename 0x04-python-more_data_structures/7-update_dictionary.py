fbb2af81646fd1da57682d257c59af33b43e388f
Enock <asanteenoch10@gmail.com>
2024-02-20 16:27:12 +0100

    update

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary = {k: value if k == key else v for k, v in a_dictionary.items()}
     for i, (k, v) in enumerate(a_dictionary.items()):
-        if k != key and i == len(a_dictionary) - 1:
-            a_dictionary[key] = value
+        if k == key:
+            k: value
             return a_dictionary
+        else:
+            a_dictionary[key] = value
     return a_dictionary
