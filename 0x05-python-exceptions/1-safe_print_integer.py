b76de6379fcc170eb17edbc4589eec831fa8e65e
Enock <asanteenoch10@gmail.com>
2024-03-01 00:12:57 +0100

    update

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
-        return True
+        print("{:d}".format(value), end="" )
+
     except ValueError:
         return False
+    finally:
+        print()
+        return True
+
