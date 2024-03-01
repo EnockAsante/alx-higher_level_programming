5766d499f5d7c5e2e70d48331cdd5c675ff32054
Enock <asanteenoch10@gmail.com>
2024-03-01 19:28:09 +0100

    update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """
+    class of Square
+    """
+    def __init__(self, size):
+        """constructor for initialising the instance variable"""
+        self.__size = size
