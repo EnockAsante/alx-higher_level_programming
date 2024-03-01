37f1b04c36b9d85bc6d7797c45deecb89fb84d48
Enock <asanteenoch10@gmail.com>
2024-03-01 19:00:14 +0100

    update

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,10 +2,5 @@
 
 class Square:
     """
-    Represents a square.
-
-    This class doesn't have any attributes or methods yet,
-    but it is intended to be a foundation for square-related
-    functionality in the future.
     """
     pass
