75d50a0f09840a1f579f400da5f373fe8b4712ff
Enock <asanteenoch10@gmail.com>
2024-03-01 20:06:33 +0100

    update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,12 +11,11 @@ class Square:
         :@size = size(private) to use"""
         try:
             if type(size) is not int:
-                raise TypeError
+                raise TypeError("size must be an integer")
             if size < 0:
-                raise ValueError
+                raise ValueError("size must be >= 0")
             else:
                 self.__size = size
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
+        except (TypeError, ValueError) as e:
+            print(e)
+
