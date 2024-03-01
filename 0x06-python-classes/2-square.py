8e8bff078d1c8b1a99e01b9c66a0452902b653a8
Enock <asanteenoch10@gmail.com>
2024-03-01 20:27:24 +0100

    update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,8 +6,7 @@ class Square:
     """
     class of Square
     """
-
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """constructor for initialising the instance variable
         :@size = size(private) to use"""
         if type(size) is not int:
@@ -16,10 +15,3 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-
-    def area(self):
-        """
-        area of a square
-        :returns square
-        """
-        return self.__size ** 2
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """
+    class of Square
+    """
+
+    def __init__(self, size = 0):
+        """constructor for initialising the instance variable
+        :@size = size(private) to use"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        area of a square
+        :returns square
+        """
+        return self.__size ** 2
