4f0b2f5be7f9a14576fcff35fbfcc462414a0cb6
Enock <asanteenoch10@gmail.com>
2024-03-01 18:44:52 +0100

    update

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    """class that define a square
-    """
-    def __init__(self):
-        """
-        constructor
-        self - instance of self
-        """
-        pass
+    """Empty class"""
+    pass
