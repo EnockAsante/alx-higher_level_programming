560826a52d6851b8f68c5e8e969340d4021000e5
Enock <asanteenoch10@gmail.com>
2024-03-05 17:35:09 +0100

    update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-def add_integer(a, b=98):
-    """add_integer
+# Filename: 0-add-integer.py
+def add_integer(a, b = 98):
+    """add_integer - add two numbers
     :@param a=lo
     :@param b=ro
-    :return:result
+    :return result
     """
     if not isinstance(a, (float, int)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (float, int)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
-
-
