eb07f4a6ea19bb55c51302d0197a8827350e3381
Enock <asanteenoch10@gmail.com>
2024-03-08 23:04:03 +0100

    update

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -12,8 +12,8 @@ def say_my_name(first_name, last_name = ""):
     :return:
     """
     for j, i in enumerate([first_name, last_name]):
-        if i:
-            if i == "None" or type(i) is not str:
-                raise (TypeError("first_name must be a string" if j == 0 else
-                                 "last_name must be a string"))
+        if i or type(i) is not str:
+            raise (TypeError("first_name must be a string" if j == 0 else
+                             "last_name must be a string"))
     print("My name is {} {}".format(first_name, last_name))
+
