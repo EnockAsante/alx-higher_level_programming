7edb405c885292d032adf62d55b4c503e8e8178d
Enock <asanteenoch10@gmail.com>
2024-03-08 22:35:33 +0100

    update

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,7 +5,7 @@ say my name
 """
 
 
-def say_my_name(first_name, last_name = ""):
+def say_my_name(first_name, last_name=""):
     """
     :param first_name:
     :param last_name:
@@ -15,7 +15,7 @@ def say_my_name(first_name, last_name = ""):
         if i == "None" or type(i) is not str:
             raise TypeError("first_name must be a string or last_name must "
                             "be a string")
-    print("My name is {}".format(first_name), end = ' ' if last_name else '')
+    print("My name is {}".format(first_name), end=' ' if last_name else '')
     if last_name:
         print("{}".format(last_name))
 
