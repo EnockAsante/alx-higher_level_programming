5f864846b1dec4d266131a57b2ce74f342d90bdf
Enock <asanteenoch10@gmail.com>
2024-03-09 00:16:05 +0100

    update

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+print_square
+"""
+
+
+def print_square(size):
+    """
+
+    :param size:
+    :return:
+    """
+    i = 0
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if isinstance(size, float):
+        if size < 0:
+            raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    else:
+        str = "#" * (size ** 2)
+        for j in (str):
+            if i == size:
+                print()
+                i = 0
+            print("{}".format(j), end ="")
+            i += 1
+        print()
+
