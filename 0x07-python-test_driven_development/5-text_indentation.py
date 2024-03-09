7532c3bc3e6f09c59a2b0f47670fc79b94d01b51
Enock <asanteenoch10@gmail.com>
2024-03-09 01:50:24 +0100

    update

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+text_indentation
+"""
+
+
+def text_indentation(text):
+    j = 0
+    """
+
+    :param text:
+    :return:
+    """
+    buffer = ""
+    if text and type(text) is str:
+        while j < len(text):
+            buffer += text[j]
+            if text[j] in '.?:':
+                buffer += '\n\n'
+                if j + 1 < len(text) and text[j + 1] == ' ':
+                    j += 1
+            j += 1
+    else:
+        raise TypeError("text must be a string")
+    print("{}".format(buffer))
+
