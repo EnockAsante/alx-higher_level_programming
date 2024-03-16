26795f35a0673c03cf0d6e1b3f5b070e652f6cbe
Enock <asanteenoch10@gmail.com>
2024-03-16 21:06:30 +0100

    update

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,30 @@
+"""Unittest for max_integer([..])
+"""
+from unittest import TestCase
+
+max_int = __import__('6-max_integer').max_integer
+
+
+class TestMax(TestCase):
+    def test_type(self):
+        self.assertRaises(TypeError, max_int, ["a", "b"])
+        self.assertRaises(TypeError, max_int, [2.3, 3.0])
+        self.assertRaises(TypeError, max_int, [1, "a", 3])
+
+    def test_max(self):
+        # Test case where all integers are positive
+        self.assertEqual(max_int([1, 3, 2]), 3)
+
+        # Test case where all integers are negative
+        self.assertEqual(max_int([-1, -3, -2]), -1)
+
+        # Test case where list contains positive and negative integers
+        self.assertEqual(max_int([-1, 3, -2]), 3)
+
+        # Test case where list contains only one integer
+        self.assertEqual(max_int([7]), 7)
+
+    def test_empty_list(self):
+        # Test case where list is empty
+        self.assertIsNone(max_int([]))
+
