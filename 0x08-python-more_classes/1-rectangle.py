ef1386b6bf16bbba52749a9b4a059d9fc043e68b
Enock <asanteenoch10@gmail.com>
2024-03-17 16:36:52 +0100

    update

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Rectangle module
+"""
+
+
+class Rectangle:
+    """
+    class Rectangle
+    """
+    __width = 0
+    __height = 0
+
+    def __init__(self, width = 0, height = 0):
+        width(width)
+        height(height)
+
+    def width(self, value):
+        if type(value) is not int:
+            raise (TypeError, "width must be an integer")
+        elif value < 0:
+            raise (ValueError, "width must be >= 0")
+        else:
+            self.__width = value
+
+    def height(self, value):
+        if type(value) is not int:
+            raise (TypeError, "height must be an integer")
+        elif value < 0:
+            raise (ValueError, "height must be >= 0")
+        else:
+            self.__width = value
+
+    def width(self):
+        return self.__width
+
+    def height(self):
+        return self.__height
