709f648eb3611e13160698113d1859b47b35d1ba
Enock <asanteenoch10@gmail.com>
2024-03-17 20:54:45 +0100

    update

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -85,9 +85,9 @@ class Rectangle:
     @classmethod
     def test_passed_cls(cls, value):
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("width must be >= 0")
         else:
             return 1
 
