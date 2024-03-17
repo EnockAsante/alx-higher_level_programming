31b0c21151d57cf042e71123c87da5f324a900c0
Enock <asanteenoch10@gmail.com>
2024-03-17 17:13:01 +0100

    update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,27 +12,37 @@ class Rectangle:
     __height = 0
 
     def __init__(self, width = 0, height = 0):
-        self.width = width
-        self.height = height
+        if self.test_passed(width, "width"):
+            self.__width = width
+        if self.test_passed(height, "height"):
+            self.__height = height
 
     def width(self, value):
-        if type(value) is not int:
-            raise (TypeError, "width must be an integer")
-        elif value < 0:
-            raise (ValueError, "width must be >= 0")
-        else:
+        if self.test_passed(value, "width"):
             self.__width = value
 
     def height(self, value):
+        if self.test_passed(value, "height"):
+            self.__width = value
+
+    def test_passed(self, value, name):
         if type(value) is not int:
-            raise (TypeError, "height must be an integer")
+            raise (TypeError, "{} must be an integer".format(name))
         elif value < 0:
-            raise (ValueError, "height must be >= 0")
+            raise (ValueError, "{} must be >= 0".format(name))
         else:
-            self.__height = value
+            return 1
 
     def width(self):
         return self.__width
 
     def height(self):
         return self.__height
+
+
+my_rectangle = Rectangle(2, 4)
+print(my_rectangle.__dict__)
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle.__dict__)
