66e6e59e041bed9f5500a64c172be0c2755224fd
Enock <asanteenoch10@gmail.com>
2024-03-23 22:27:21 +0100

    update

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+mod Square
+"""
+Rect = __import__('9-rectangle').Rectangle
+
+
+class Square(Rect):
+    """
+    Square
+    """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__width, self.__height)
