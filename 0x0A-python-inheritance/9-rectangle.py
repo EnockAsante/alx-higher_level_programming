60fb2a0c06348a0c86b604ed0b422360a44866ae
Enock <asanteenoch10@gmail.com>
2024-03-23 21:50:16 +0100

    update

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+mod Base
+"""
+BaseG = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseG):
+    """
+    Rectangle
+    """
+
+    def __init__(self, width, height):
+        super().__init__()
+        if self.integer_validator("width", width):
+            self.__width = width
+        if self.integer_validator("height", height):
+            self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
