7eeca824a3efbc43d8d90d1b9cc18eb528c3bb51
Enock <asanteenoch10@gmail.com>
2024-03-23 19:56:05 +0100

    update

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+"""
+mod same type
+"""
+def is_same_class(obj, a_class):
+    """
+
+    :param obj:
+    :param a_class:
+    :return:
+    """
+    return True if type(obj) is a_class else False
