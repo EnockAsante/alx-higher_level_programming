ecadccae8e1eeef5c572f377620b8405e1caea2e
Enock <asanteenoch10@gmail.com>
2024-03-24 15:04:17 +0100

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,11 @@ read_file mod
 
 
 def read_file(filename = ""):
+    """
+
+    :param filename:
+    :return:
+    """
     with open(filename, 'r', encoding = "UTF8") as f:
         for line in f:
             print("{}".format(line), end = "")
