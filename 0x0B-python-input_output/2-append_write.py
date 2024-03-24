25d7002a753cec20a1be33d76e1b79062936ff3d
Enock <asanteenoch10@gmail.com>
2024-03-24 15:17:18 +0100

    update

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+mod append file
+"""
+
+
+def append_write(filename="", text=""):
+    """
+
+    :param filename:
+    :param text:
+    :return:
+    """
+    with open(filename, "a", encoding = "UFT8") as f:
+        return f.write(text)
