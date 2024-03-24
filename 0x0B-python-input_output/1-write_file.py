b83f62b9843d55989454f8986a838368c1c025de
Enock <asanteenoch10@gmail.com>
2024-03-24 15:08:52 +0100

    update

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+"""
+mod write file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+
+    :param filename:
+    :param text:
+    :return:
+    """
+    with open(filename, "w", encoding="UTF8") as f:
+        f.write(text)
