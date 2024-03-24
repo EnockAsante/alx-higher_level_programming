776a882edcf2a5edc6f8aeb9b37e9c145e868946
Enock <asanteenoch10@gmail.com>
2024-03-24 16:07:40 +0100

    update

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -11,5 +11,5 @@ def load_from_json_file(filename):
     :param filename:
     :return:
     """
-    with open(filename, "r", encoding = "UTF8") as f:
+    with open(filename, "r", encoding="UTF8") as f:
         return json.loads(f.read())
