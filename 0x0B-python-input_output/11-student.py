58a406eddaf173bfde6492c2b315bd72e76cf1bf
Enock <asanteenoch10@gmail.com>
2024-03-24 19:07:55 +0100

    update

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+mod class to json
+"""
+
+
+class Student:
+    def __init__(self, fname, lname, age):
+        self.first_name = fname
+        self.last_name = lname
+        self.age = age
+
+    def to_json(self, attrs=None):
+        dic = vars(self)
+        new_dic = {}
+        if type(attrs) is list:
+            for item in attrs:
+                for k, v in dic.items():
+                    if item == k:
+                        new_dic[k] = v
+            return new_dic
+        else:
+            return dic
+
+    def reload_from_json(self, json):
+        dic = vars(json)
+        for k, v in dic.items():
+            if k == "first_name":
+                self.first_name = v
+            if k == "last_name":
+                self.last_name = v
+            if k == "age":
+                self.age = v
