135907fb5453f72d809901467b3e0770c5d487fb
Enock <asanteenoch10@gmail.com>
2024-03-24 16:41:59 +0100

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,10 +6,12 @@ from sys import argv
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-
-new_list = load_from_json_file("add_item.json")
-if not new_list:
-    new_list = []
+new_list = []
+try:
+    new_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    with open("add_item.json", "w", encoding = "UTF8") as f:
+        pass
 for idx, arg in enumerate(argv):
     if idx > 0:
         new_list.append(arg)
