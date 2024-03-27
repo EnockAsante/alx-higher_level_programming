36dcf011f0b6b2a3ac615d5e973ac49d4fcc240b
Enock <asanteenoch10@gmail.com>
2024-03-27 18:57:15 +0100

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -130,3 +130,19 @@ class Rectangle(Base):
         for k, v in kwargs.items():
             self.validator(k, v)
             setattr(self, k, v)
+
+    def to_dictionary(self):
+        return vars(self)
+
+
+r1 = Rectangle(10, 2, 1, 9)
+print(r1)
+r1_dictionary = r1.to_dictionary()
+print(r1_dictionary)
+print(type(r1_dictionary))
+
+r2 = Rectangle(1, 1)
+print(r2)
+r2.update(**r1_dictionary)
+print(r2)
+print(r1 == r2)
