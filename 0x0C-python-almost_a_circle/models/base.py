03a46c00f2cc335a8ce0007ed786d11a2b7b8db6
Enock <asanteenoch10@gmail.com>
2024-03-27 19:19:52 +0100

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,3 +16,8 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if not list_dictionaries:
+            return "[]"
+        return str(list_dictionaries)
