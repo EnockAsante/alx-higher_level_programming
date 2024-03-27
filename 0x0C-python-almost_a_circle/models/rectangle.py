8032753dc8884aefb018ae822160e0f425127ffb
Enock <asanteenoch10@gmail.com>
2024-03-27 18:58:05 +0100

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -133,16 +133,3 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         return vars(self)
-
-
-r1 = Rectangle(10, 2, 1, 9)
-print(r1)
-r1_dictionary = r1.to_dictionary()
-print(r1_dictionary)
-print(type(r1_dictionary))
-
-r2 = Rectangle(1, 1)
-print(r2)
-r2.update(**r1_dictionary)
-print(r2)
-print(r1 == r2)
